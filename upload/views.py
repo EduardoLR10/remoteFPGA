395235b4f056bd8d77fdd780ad63de44b14176ae
r-fsantos <felicio95.jr@gmail.com>
2021-02-26 19:54:34 -0300

    Code formating due PEP8 recomendations

--- upload/views.py
+++ upload/views.py
@@ -5,9 +5,11 @@ from . import script
 
 from django.contrib.auth.decorators import login_required
 
+
 @login_required(login_url='login')
 def queue(request):
     return render(request, 'upload/queue.html')
+
 
 @login_required(login_url='login')
 def upload(request):
@@ -23,4 +25,5 @@ def upload(request):
         fs = FileSystemStorage()
         fs.save(uploaded_file.name, uploaded_file)
         script.openImage(uploaded_file.name)
+
     return render(request, 'upload/upload.html')
