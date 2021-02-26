93b895afaa92031d1e8a3e273e616b1aeca76727
r-fsantos <felicio95.jr@gmail.com>
2021-02-26 19:53:09 -0300

    Adding some TODOs for debating with the other devs

--- upload/views.py
+++ upload/views.py
@@ -12,6 +12,13 @@ def queue(request):
 @login_required(login_url='login')
 def upload(request):
     if request.method == "POST":
+        # TODO: Write method, in a folder at this app (tools/, for instance),
+        #  to validate the input file
+
+        # TODO: Its important to keep this file during runtime only?
+        # TODO: How long will take "runtime", there is, how long will
+        #  the user be using the application?
+
         uploaded_file = request.FILES['document']
         fs = FileSystemStorage()
         fs.save(uploaded_file.name, uploaded_file)
